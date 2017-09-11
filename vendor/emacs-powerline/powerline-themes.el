85419af31d6c190e93b488970490e0977cd78a93
Puneet Pahuja <er.puneetpahuja@gmail.com>
2017-09-11 12:17:52 +0530

    Update

--- vendor/emacs-powerline/powerline-themes.el
+++ vendor/emacs-powerline/powerline-themes.el
@@ -40,7 +40,7 @@
                           (face1 (if active 'powerline-active1 'powerline-inactive1))
                           (face2 (if active 'powerline-active2 'powerline-inactive2))
                           (separator-left (intern (format "powerline-%s-%s"
-                (powerline-current-separator)
+                                                          (powerline-current-separator)
                                                           (car powerline-default-separator-dir))))
                           (separator-right (intern (format "powerline-%s-%s"
                                                            (powerline-current-separator)
@@ -68,19 +68,19 @@
                                        (powerline-raw (list (nyan-create)) face2 'l))))
                           (rhs (list (powerline-raw global-mode-string face2 'r)
                                      (funcall separator-right face2 face1)
-             (unless window-system
-               (powerline-raw (char-to-string #xe0a1) face1 'l))
-             (powerline-raw "%4l" face1 'l)
-             (powerline-raw ":" face1 'l)
-             (powerline-raw "%3c" face1 'r)
-             (funcall separator-right face1 mode-line)
-             (powerline-raw " ")
-             (powerline-raw "%6p" mode-line 'r)
+                                     (unless window-system
+                                       (powerline-raw (char-to-string #xe0a1) face1 'l))
+                                     (powerline-raw "%4l" face1 'l)
+                                     (powerline-raw ":" face1 'l)
+                                     (powerline-raw "%3c" face1 'r)
+                                     (funcall separator-right face1 mode-line)
+                                     (powerline-raw " ")
+                                     (powerline-raw "%6p" mode-line 'r)
                                      (when powerline-display-hud
                                        (powerline-hud face2 face1)))))
-         (concat (powerline-render lhs)
-           (powerline-fill face2 (powerline-width rhs))
-           (powerline-render rhs)))))))
+                     (concat (powerline-render lhs)
+                             (powerline-fill face2 (powerline-width rhs))
+                             (powerline-render rhs)))))))
 
 ;;;###autoload
 (defun powerline-center-theme ()
@@ -255,28 +255,30 @@
                  (powerline-current-separator)
                  (cdr powerline-default-separator-dir))))
         (lhs
-         (list (if (buffer-modified-p)
-                 (powerline-raw "  + " mode-line)
+         (list (powerline-raw "  " mode-line)
+               (if (buffer-modified-p)
+                 (powerline-raw "+" mode-line)
                  (if buffer-read-only
-                   (powerline-raw "  RO " mode-line)
+                   (powerline-raw "RO" mode-line)
                    ;(powerline-raw "  %z " mode-line)
-                   (powerline-raw "  = " mode-line)))
+                   (powerline-raw "=" mode-line)))
                (powerline-buffer-id `(mode-line-buffer-id ,mode-line) 'l)
 
 
                (when (and vc-mode buffer-file-name)
                  (let ((backend (vc-backend buffer-file-name)))
                    (when backend
-                     (concat (powerline-raw " " mode-line 'l)
-                             (powerline-raw (format "|-%s" (vc-working-revision buffer-file-name backend)))
+                     (concat ;;(powerline-raw " " mode-line 'l)
+                             (powerline-raw "|-" mode-line 'l)
+                             (powerline-raw (vc-working-revision buffer-file-name backend) mode-line 'l)
                              ;; (powerline-raw " " mode-line)
                              ))))))
         (rhs (list ;(powerline-raw '(10 "%i"))
                    (powerline-raw global-mode-string mode-line 'r)
                    (powerline-raw "[%l," mode-line 'l)
-                   (powerline-raw (format-mode-line '(3 "%c] ")))
+                   (powerline-raw "%c] " mode-line 'l)
                    (powerline-raw (replace-regexp-in-string  "%" "%%" (format-mode-line '(-3 "%p"))) mode-line 'r)
-                   (powerline-raw " " mode-line 'l)
+                   ;;(powerline-raw " " mode-line 'l)
                    (powerline-major-mode mode-line)
                    (powerline-process mode-line)
                    ;; (powerline-raw " " mode-line)
@@ -290,8 +292,9 @@
                    (powerline-raw "%n" mode-line)
                    (powerline-raw "]" mode-line))))
          (concat (powerline-render lhs)
-            ;(powerline-fill mode-line (powerline-width rhs))
-           (powerline-render rhs))
+                 (powerline-render rhs)
+                 ;; (powerline-fill mode-line (powerline-width rhs))
+                 )
          )))))
 
 ;;;###autoload
