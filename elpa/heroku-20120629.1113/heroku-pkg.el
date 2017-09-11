fcabf8c56e0ab5890b7ec3e4a6b210d64e81ed95
Puneet Pahuja <er.puneetpahuja@gmail.com>
2017-09-11 11:49:34 +0530

    Update

---
+++ elpa/heroku-20120629.1113/heroku-pkg.el
@@ -0,0 +1,1 @@
+(define-package "heroku" "20120629.1113" "Interface to Heroku apps." 'nil :commit "92af1c073b593c4def99c8777c869992aa4d0b3a" :url "https://github.com/technomancy/heroku.el" :keywords '("convenience" "api" "database"))
